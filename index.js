fa953d34ccad75a9e37368f7376ff6bf72e3c9c4
Patrick Lee <patrick.lorian.lee@gmail.com>
2014-08-07 20:30:48 -0500

    look in the package.json as well for brDirs, add method for testing

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 var path = require('path'),
     join = path.join,
-    rootDir = path.resolve(join(__dirname,'../../')),
-    brConfig, brFileName = 'br';
+    rootDir = path.resolve(join(__dirname, '../../')),
+    brConfig, brFileName = 'br', packageJsonNode = 'brDirs'
 
-try {
-    brConfig = require(join(rootDir, brFileName));
-} catch (e) {}
+/**
+ * Try to read config from br.js or br.json and then look under
+ * the package.json to see if it is in the brDirs node
+ */
+(function initBrConfig() {
+    try {
+        brConfig = require(join(rootDir, brFileName));
+    } catch (e) {
+        try {
+            brConfig = require(join(rootDir, 'package.json'))[packageJsonNode];
+        } catch (e2) {}
+    }
+})();
+
 
 function getFileName(fileName) {
     var fileSplit, pathKey, pathValue;
@@ -24,3 +35,12 @@ function getFileName(fileName) {
 module.exports = function(fileName) {
     return require(join(rootDir, getFileName(fileName)));
 };
+
+/**
+ * Add method to help test
+ * @private
+ */
+module.exports._setRootDir = function(dir) {
+    rootDir = dir;
+    initBrConfig();
+}
